8603c267b69f2003f616ac746ba0c50c69be027f
Alan Hong <xiupan@gmail.com>
2017-07-09 10:35:05 -0500

    Dynamically get Drug Name from the HTML and do a fetch for that name!

--- public/contradrug.js
+++ public/contradrug.js
@@ -1,18 +1,21 @@
-fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?name=omeprazole`) // use QuerySelector to get and set drug search
-  .then(function (response) {
-    return response.json();
+var temp = document.querySelector(".drugName").innerHTML;
+console.log(temp);
+
+fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${temp}`) // use QuerySelector to get and set drug search
+.then(function (response) {
+  return response.json();
 }).then(function (json) {
-    return json.idGroup.rxnormId[0];
+  return json.idGroup.rxnormId[0];
 }).then(function (medID) {
   fetch(`https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=${medID}&sources=ONCHigh`)
-    .then(function (res2) {
-      return res2.json();
+  .then(function (res2) {
+    return res2.json();
   }).then(function (contra) {
-      var contraDrug = contra.interactionTypeGroup[0].interactionType[0].interactionPair[0].description;
-      console.log(contraDrug);
-      injectHTML(contraDrug);
-      function injectHTML(html){
-        document.querySelector(".contraindicator-1").insertAdjacentHTML('afterbegin', html);
-      }
+    var contraDrug = contra.interactionTypeGroup[0].interactionType[0].interactionPair[0].description;
+    console.log(contraDrug);
+    injectHTML(contraDrug);
+    function injectHTML(html){
+      document.querySelector(".contraindicator-1").insertAdjacentHTML('afterbegin', html);
+    }
   })
 })
