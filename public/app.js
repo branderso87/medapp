b913bc6f908a26b5c73b3944b3ba4721f97c400b
Alan Hong <xiupan@gmail.com>
2017-07-09 09:33:20 -0500

    Contra-indications Fetch cleaned up a bit.

--- public/app.js
+++ public/app.js
@@ -1,7 +1,4 @@
-//fetch for patient meds
-// looping through array of meds and push to contra fetch
-//fetch for contraindications
-fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?name=omeprazole`)
+fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?name=omeprazole`) // use QuerySelector to get and set drug search
   .then(function (response) {
     return response.json();
 }).then(function (json) {
@@ -15,8 +12,3 @@ fetch(`https://rxnav.nlm.nih.gov/REST/rxcui.json?name=omeprazole`)
       console.log(contraDrug);
   })
 })
-
-// fetch for patient list
-// fetch for med refills
-// fetch for authorization
-// console.log('Poop');
