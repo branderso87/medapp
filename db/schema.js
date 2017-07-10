80df551f6a9497cb19618512f5a6cf50c38568ab
colonelrascals <pdhildreth@gmail.com>
2017-07-10 10:01:43 -0500

    formatting

--- db/schema.js
+++ db/schema.js
@@ -3,31 +3,42 @@ const mongoose = require('mongoose');
 const medSchema = new mongoose.Schema({
     name: {
       type: String,
-      required: true },
+      required: true
+    },
     //no need to have required: false
-    rxcui: { type: Number },
-    contraAll: { type: String }
+    rxcui: {
+      type: Number
+    },
+    contraAll: {
+      type: String
+    }
 })
 
 const userSchema = new mongoose.Schema({
     firstName: {
       type: String,
-      required: true },
+      required: true
+    },
     lastName: {
       type: String,
-      required: true },
+      required: true
+    },
     dob: {
       type: Date,
-      required: true },
+      required: true
+    },
     email: {
       type: String,
-      required: true },
+      required: true
+    },
     memberId: {
       type: Number,
-      required: true },
+      required: true
+    },
     cardholder: {
       type: Boolean,
-      default: true }
+      default: true
+    }
 })
 
 model.exports = {
